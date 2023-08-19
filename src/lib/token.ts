655a2f2f3ce7243a5ff8a28a3c1f39a914101bf3
tayyab hussain <tayyabhussain496@gmail.com>
2023-08-19 13:15:39 +0500

    created verifyJWT function

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -19,3 +19,18 @@ export const signJWT = async (
     throw error;
   }
 };
+
+import { jwtVerify } from "jose";
+export const verifyJWT = async <T>(token: string): Promise<T> => {
+  try {
+    return (
+      await jwtVerify(
+        token,
+        new TextEncoder().encode(process.env.JWT_SECRET_KEY)
+      )
+    ).payload as T;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Your token has expired.");
+  }
+};
