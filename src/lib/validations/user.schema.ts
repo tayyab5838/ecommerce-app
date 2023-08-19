7898f660c6cd04f5bfcbefcd32121810eb97b69d
tayyab hussain <tayyabhussain496@gmail.com>
2023-08-19 13:17:07 +0500

    created Login User Schema

--- src/lib/validations/user.schema.ts
+++ src/lib/validations/user.schema.ts
@@ -26,3 +26,17 @@ export const RegisterUserSchema = z
   });
 
 export type RegisterUserInput = z.infer<typeof RegisterUserSchema>;
+
+export const LoginUserSchema = z.object({
+  email: z
+    .string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Email is invalid"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password at least must be 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+});
+
+export type LoginUserInput = z.infer<typeof LoginUserSchema>;
